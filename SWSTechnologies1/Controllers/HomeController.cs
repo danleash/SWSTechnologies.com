6271fe4e29e44fcda3c7d8578b42090c2f36ddb5
Daniel Leash <dan.leash0596@gmail.com>
2019-08-17 14:07:21 -0400

    Add project files.

---
+++ SWSTechnologies1/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SWSTechnologies1.Models;
+
+namespace SWSTechnologies.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View("HomePage");
+        }
+        public ViewResult ContactUs()
+        {
+            return View("ContactUs");
+        }
+        public ViewResult About()
+        {
+            return View("About");
+        }
+        public ViewResult Testimonials()
+        {
+            return View("Testimonials");
+        }
+
+    }
+}
